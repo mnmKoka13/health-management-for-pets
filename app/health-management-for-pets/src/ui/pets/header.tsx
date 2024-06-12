ff1370bc21b3fd5c76fef616030ef21a8b412643
Manami Kokado <kokado313.mnm@gmail.com>
2024-06-13 00:18:18 +0900

    TOP画面仮実装完了

--- app/health-management-for-pets/src/ui/pets/header.tsx
+++ app/health-management-for-pets/src/ui/pets/header.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 export default function Header() {  
   return (
-    <header className="bg-amber-200 text-gray-600 body-font">
-      <div className="container flex flex-wrap p-3 flex-row justify-between items-center">
+    <header id="header" className="bg-amber-300 text-gray-600 body-font">
+      <div className="container flex flex-wrap px-3 py-2 flex-row justify-between items-center">
         <a className="flex title-font font-medium items-center text-gray-900 md:mb-0">
           <Image src="/sample-icon.png" alt="icon" width={50} height={50} />
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-lg">ペットの健康管理手帳</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">ユーザー設定</a>
+          <Image src="/bars_hoso.svg" alt="icon" width={40} height={40} />
         </nav>
       </div>
     </header>
---
+++ app/health-management-for-pets/src/ui/pets/petTable.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+
+export default function PetTable() {
+  const pets = ["すあま", "ナッツ", "いろは", "ふみ", "竹千代"];
+
+  const rows = pets.map((pet) => 
+    <tr key={pet}>
+      <td className="px-4 py-3 w-1/6">
+        <Image src="/sample-icon.png" alt="icon" width={50} height={50} />
+      </td>
+      <td className="px-4 py-3 text-base w-1/2">{pet}</td>
+      <td className="text-center">
+        <button className="flex ml-auto text-white text-sm bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-teal-700 rounded">詳細</button>
+      </td>
+    </tr>
+  );
+  
+  return (
+    <table className="table-auto w-full text-left whitespace-no-wrap">
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  );
+};
